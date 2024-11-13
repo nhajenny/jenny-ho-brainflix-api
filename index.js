d87bccdb203f8f5f80f5e9cf5823bc5175975e31
Jenny Ho <nha.jennyho@gmail.com>
2024-11-13 01:04:38 -0500

    fix import dot env error

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import videoRoutes from './routes/videos.js';
 import cors from 'cors';
-import 'dontenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = 8080;
 const {CORS_ORIGIN} = process.env;
 
 app.use(bodyParser.json());
 app.use('/videos', videoRoutes);
 app.use(cors({CORS_ORIGIN}));
+
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
