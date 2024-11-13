ff0e868e9d893a2b5c6f3a6f825d7920e212d64a
Jenny Ho <nha.jennyho@gmail.com>
2024-11-13 00:29:14 -0500

    add cors to index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import videoRoutes from './routes/videos.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
 
-
 app.use(bodyParser.json());
-
-
 app.use('/videos', videoRoutes);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
